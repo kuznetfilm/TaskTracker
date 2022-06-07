b70bdcb16a64200f2c1fe0cee91b6eafad82c1e5
kuznetfilm <kuznetfilm@inbox.ru>
2022-06-07 19:45:58 +0300

    домашка спринт 3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Manager daily = new Manager();
-        SuperManager superDaily = new SuperManager();
+        Manager manager = new Manager();
+        SuperManager superManager = new SuperManager();
         Scanner scanner = new Scanner(System.in);
         Scanner scanner1 = new Scanner(System.in);
 
@@ -13,40 +13,40 @@ public class Main {
             int command = scanner.nextInt();
             switch (command) {
                 case 1:
-                    daily.createNewTask();
+                    manager.createNewTask();
                     break;
                 case 11:
-                    daily.printAllTasks();
+                    manager.printAllTasks();
                     break;
                 case 12:
-                    daily.eraseAllTasks();
+                    manager.eraseAllTasks();
                     break;
                 case 13:
                     int taskNumber = scanner.nextInt();
-                    daily.showTheTask(taskNumber);
+                    manager.showTheTask(taskNumber);
                     break;
                 case 14: //удалить задачу
                     System.out.println(" введите номер задачи");
                     int taskNumber1 = scanner.nextInt();
-                    daily.removeTheTask(taskNumber1);
+                    manager.removeTheTask(taskNumber1);
                     break;
                 case 15:     // изменить статус задачи
                     System.out.println(" введите номер задачи");
                     int taskNumber2 = scanner.nextInt();
                     System.out.println(" введите новый статус");
                     String status1 = scanner1.next();
-                    daily.allTasks.get(taskNumber2).statusOfTask = status1;
+                    manager.allTasks.get(taskNumber2).statusOfTask = status1;
                     break;
                 case 2:
-                    daily.createNewEpic();
+                    manager.createNewEpic();
                     break;
                 case 21: //вывести эпик по номеру
                     System.out.println(" введите номер эпика");
                     int epicNumb = scanner.nextInt();
-                    daily.printEpicByNumber(epicNumb);
+                    manager.printEpicByNumber(epicNumb);
                     break;
                 case 22:    // печать всех эпиков
-                    daily.printAllEpics();
+                    manager.printAllEpics();
                     break;
                 case 23:     ///// добавить подзадачу в эпик
                     System.out.println(" введите номер эпика");
@@ -55,7 +55,7 @@ public class Main {
                     String nameOfSubtask = scanner1.next();
                     System.out.println(" введите детали задачи");
                     String detailsOfSubtask = scanner1.next();
-                    daily.addSubtaskToEpic(epicNumb1, nameOfSubtask, detailsOfSubtask);
+                    manager.addSubtaskToEpic(epicNumb1, nameOfSubtask, detailsOfSubtask);
                     break;
                 case 24: {   // изменить статус подзадачи
                     System.out.println(" введите номер эпика");
@@ -65,28 +65,28 @@ public class Main {
                     System.out.println(" введите новый статус");
                     String status = scanner1.next();
 
-                    daily.changeSubTuskStatus(epicNumb2, subTaskNumber,status);
+                    manager.changeSubTuskStatus(epicNumb2, subTaskNumber,status);
                     break;
                 }
                 case 25: //эпики по статусам
                     System.out.println(" какой статус вы хотите посмотреть ?");
                     String status = scanner.next();
-                    daily.printEpicWithThisStatus(status);
+                    manager.printEpicWithThisStatus(status);
                     break;
                 case 26: // удалить эпик
                     System.out.println(" введите номер эпика");
                     int epicNumb3 = scanner.nextInt();
-                    daily.allEpics.remove(epicNumb3);
+                    manager.allEpics.remove(epicNumb3);
                     break;
                 case 31: // обновить эпик
                     System.out.println(" введите номер эпика");
                     int epicNumb4 = scanner.nextInt();
-                    superDaily.updateEpic( daily.allEpics.get(epicNumb4) );
+                    superManager.updateEpic( manager.allEpics.get(epicNumb4) );
                     break;
                 case 32: // обновить задачу
                     System.out.println(" введите номер задачи");
                     int taskNumb5 = scanner.nextInt();
-                    superDaily.updateTask(daily.allTasks.get(taskNumb5));
+                    superManager.updateTask(manager.allTasks.get(taskNumb5));
                     break;
                 case 9:
                     System.exit(0);
